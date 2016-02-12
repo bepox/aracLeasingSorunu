322abeb106eab4990f277319dd0370a5607fd255
bepox <eraykuyumcu@gmail.com>
2016-02-12 17:45:35 +0100

    variables changes to temp...

---
+++ src/multidimensDemo.java
@@ -0,0 +1,25 @@
+/**
+ * Created by kuyum on 12-Feb-16.
+ */
+public class multidimensDemo {
+    public static void main (String args[]) {
+        int sqrs [] [] = {
+                {1,1},
+                {2,4},
+                {3,9},
+                {4,16},
+                {5,25},
+                {6,36},
+                {7,49},
+                {8,64},
+                {9,81},
+                {10,100},
+        };
+
+        int i, j;
+        for (i=0; i < 10; i++) {
+            for (j =0; j < 1; j++)
+                System.out.println();
+        }
+    }
+}
